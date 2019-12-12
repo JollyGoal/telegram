037c7bbee2d5acaa8af057df004cef569071d806
JollyGoal <stfill01list.ru@gmail.com>
2019-12-12 21:53:38 +0500

    bot add

--- telegram/bot/views.py
+++ telegram/bot/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class GetList(APIView):
+    def get(self,request):
+        pass
+
+    def post(self, request):
+        pass
