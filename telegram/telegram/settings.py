62f364faaa32b7280ae99d15b553c73ab48b27b1
JollyGoal <stfill01list.ru@gmail.com>
2019-12-27 22:00:28 +0500

    DDDD

--- telegram/telegram/settings.py
+++ telegram/telegram/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+os.makedirs(STATIC_TMP, exist_ok=True)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
